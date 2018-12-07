2bb91eb147f6d41090752e3f39f758289195d65b
nadezhdakharchuk <nadezhda.kharchuk@flatstack.com>
2018-12-07 10:39:18 +0300

    add css linter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
 import './App.css';
 
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
 const App = () => (
-  <div className="App">
+  <Wrapper>
     <header className="App-header">
       Rewards
     </header>
-  </div>
+  </Wrapper>
 );
 
 export default App;
