4d6a8389d44f421c5a0ae34907b19914cfbd29d3
nadezhdakharchuk <nadezhda.kharchuk@flatstack.com>
2018-12-13 12:01:38 +0300

    add some styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import logo from './images/logo.png';
 
 const Wrapper = styled.div`
   display: flex;
@@ -7,14 +8,46 @@ const Wrapper = styled.div`
 `;
 
 const Header = styled.div`
+  position: relative;
   display: flex;
-  width: 100%;
+  align-items: center;
+  padding: 20px 15px;
+  z-index: 10;
+`;
+
+const LogoWrapper = styled.a`
+  display: inline-flex;
+  align-items: center;
+  padding-top: 0;
+  padding-bottom: 0;
+`;
+
+const LogoIcon = styled.span`
+  display: inline-block;
+  width: 30px;
+  height: 30px;
+  background-image: url(${logo});
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: contain;
+`;
+
+const LogoText = styled.span`
+  display: inline-block;
+  font-size: 20px;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: 800;
+  padding: 0 10px;
 `;
 
 const App = () => (
   <Wrapper>
     <Header>
-      Rewards
+      <LogoWrapper>
+        <LogoIcon />
+        <LogoText>Rewards</LogoText>
+      </LogoWrapper>
     </Header>
   </Wrapper>
 );
