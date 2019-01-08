bb89060072661feb1317d243d2d926ae4acf520d
Ilgiz Gilmanov <ilgiz.gilmanov@flatstack.com>
2019-01-08 12:30:12 +0300

    get token test

---
+++ src/userTokens/getToken.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const apiUrl = 'https://rewards.flatstack.com/api/v1/user/tokens';
+
+export default async (email, password) => {
+
+  const response = await axios.post(apiUrl, {
+    data: {
+      data: {
+        type: 'user-token-requests',
+        attributes: {
+          email,
+          password,
+        },
+      },
+    },
+  });
+
+  return response.data.data.attributes.token;
+};
