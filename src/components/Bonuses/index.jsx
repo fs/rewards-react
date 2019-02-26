37e3091e66d8e251dde2b8fdf8064ce6cd3d23ac
Ilgiz Gilmanov <ilgiz.gilmanov@flatstack.com>
2019-02-26 12:01:18 +0300

    create sendBonusForm

--- src/components/Bonuses/index.jsx
+++ src/components/Bonuses/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 import MainTemplate from '../templates/MainTemplate';
+import SendBonusForm from '../organisms/SendBonusForm';
 
 const BonusMainWrap = styled.div`
   display: flex;
   width: 60%;
   margin: 100px auto 0;
-
 `;
 
-const DashboardForm = styled.div`
+const BonusContent = styled.div`
   margin-bottom: 1.25rem;
   padding: 1.25rem;
   box-shadow: 0 1px 2px 0 rgba(0, 0, 0, .1);
@@ -20,9 +20,11 @@ const DashboardForm = styled.div`
 const Bonuses = () => (
   <MainTemplate>
     <BonusMainWrap>
-      <DashboardForm>
+      <BonusContent>
         <h2>Bonuses</h2>
-      </DashboardForm>
+        <SendBonusForm></SendBonusForm>
+      </BonusContent>
+      {/* <BonusRating></BonusRating> */}
     </BonusMainWrap>
   </MainTemplate>
 );
---
+++ src/components/organisms/SendBonusForm/index.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Form = styled.form`
+  width: 100%;
+  background-color: #fff;
+  border-radius: 5px;
+  padding: 20px;
+`;
+
+const Textarea = styled.textarea`
+  font-size: 1.25rem;
+  font-weight: 400;
+  line-height: 1.75rem;
+  color: #000;
+  border: none;
+  outline: none;
+  padding: 0;
+  height: 3.5rem;
+  margin: 0 0 0.625rem 0;
+  resize: none;
+  box-shadow: none;
+`;
+
+const Button = styled.button`
+  background-color: #63bc36;
+  color: #fff;
+  border-radius: 100px;
+  height: 40px;
+  line-height: 40px;
+  padding: 0 20px;
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+class SendBonusForm extends Component {
+  render() {
+    return (
+      <Form>
+        <Textarea placeholder="+100 @person add description for #create_awesomness">
+
+        </Textarea>
+        <Button>Give</Button>
+      </Form>
+    );
+  }
+}
+
+export default SendBonusForm;
