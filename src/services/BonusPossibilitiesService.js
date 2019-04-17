985a5b9c0357d95fce35a4d4ae388b9722616b40
nadezhdakharchuk <nadezhda.kharchuk@flatstack.com>
2019-04-17 12:35:10 +0300

    refactor code

--- src/services/BonusPossibilitiesService.js
+++ src/services/BonusPossibilitiesService.js
@@ -1,23 +1,43 @@
 import api from './ApiService';
 
 const apiUrl = '/user/bonus_possibilities';
+const POINTS = 'points';
+const TAGS = 'tags';
+const USERS = 'users';
 
 export default class BonusPossibilitiesService {
-  static POINTS = 'points';
-
-  static TAGS = 'tags';
-
-  static USERS = 'users';
-
   static async savePossibilities(token) {
     const possibilities = await BonusPossibilitiesService.fetchPosibilities(token);
-    const points = JSON.stringify(possibilities.data.included.filter(item => item.type === BonusPossibilitiesService.POINTS));
-    const tags = JSON.stringify(possibilities.data.included.filter(item => item.type === BonusPossibilitiesService.TAGS));
-    const users = JSON.stringify(possibilities.data.included.filter(item => item.type === BonusPossibilitiesService.USERS));
 
-    localStorage.setItem(BonusPossibilitiesService.POINTS, points);
-    localStorage.setItem(BonusPossibilitiesService.TAGS, tags);
-    localStorage.setItem(BonusPossibilitiesService.USERS, users);
+    const points = possibilities.data.included
+      .filter(item => item.type === POINTS)
+      .map(item => ({
+        id: item.id,
+        value: item.attributes.value,
+      }));
+
+    const tags = possibilities.data.included
+      .filter(item => item.type === TAGS)
+      .map(item => ({
+        id: item.id,
+        label: item.attributes.label,
+      }));
+
+    const users = possibilities.data.included
+      .filter(item => item.type === USERS)
+      .map(item => ({
+        id: item.id,
+        email: item.attributes.email,
+        username: item.attributes.username,
+        'full-name': item.attributes['full-name'],
+        'allowance-balance': item.attributes['allowance-balance'],
+        'bonus-balance': item.attributes['bonus-balance'],
+        'profile-image-avatar-url': item.attributes['profile-image-avatar-url'],
+      }));
+
+    localStorage.setItem(POINTS, JSON.stringify(points));
+    localStorage.setItem(TAGS, JSON.stringify(tags));
+    localStorage.setItem(USERS, JSON.stringify(users));
   }
 
   static async fetchPosibilities(token) {
