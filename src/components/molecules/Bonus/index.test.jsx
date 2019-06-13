701f2ea3ade0f31dcdac8814ad6de170f013264c
hex22a <hex22a@gmail.com>
2019-06-13 11:50:45 +0300

    Bonus index shallow render example

--- src/components/molecules/Bonus/index.test.jsx
+++ src/components/molecules/Bonus/index.test.jsx
@@ -1,29 +1,20 @@
 import 'jest-dom/extend-expect';
-import 'react-testing-library/cleanup-after-each';
-import TimeAgo from 'timeago-react';
+import ShallowRenderer from 'react-test-renderer/shallow';
 
 import React from 'react';
 
-import { render } from 'react-testing-library';
 import Bonus from '.';
 import bonusList from '../../../mock_data/bonusList';
-
-jest.mock('timeago-react');
 
 describe('Bonus index', () => {
   test('render Bonus correctly', () => {
     // Arrange
-    const expectedDateTime = '2019-06-11T15:35:22.447Z';
-    // eslint-disable-next-line react/prefer-stateless-function
-    const mockTimeAgo = class TimeAgo extends React.Component {
-      render() {
-        return <TimeAgo datetime={expectedDateTime} locale="en_US" />;
-      }
-    };
-    TimeAgo.mockImplementation(mockTimeAgo);
+    const renderer = new ShallowRenderer();
+    renderer.render(<Bonus bonus={bonusList[0]} />);
     // Act
-    const wrapper = render(<Bonus bonus={bonusList[0]} />);
+    const result = renderer.getRenderOutput();
+
     // Assert
-    expect(wrapper).toMatchSnapshot();
+    expect(result).toMatchSnapshot();
   });
 });
